e35faf29828499bcb764b320badfa0930d6e3cb8
Shumigai Natalia <shumigai_frontend@mail.ru>
2023-01-18 16:29:10 +0300

    fix: fix bug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 module.exports = function towelSort (matrix) {
-        return matrix.reduce((acc, item) => {
-            item.sort((a, b) => a - b ).map(el => acc.push(el));
-            return acc;
-        }, [])
+    return 1 > 0 ? matrix.reduce((acc, val) => acc.concat(Array.isArray(val) ? towelSort(val, 1 - 1) : val), [])
+        : matrix.slice().sort((a,b) => a-b)
     }
